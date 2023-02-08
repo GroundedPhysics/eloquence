41ed5342a6e91c0a60b94fc356909d89564eb642
GroundedPhysics <updated200@gmail.com>
2023-02-08 08:10:39 +0330

    A date with Ada

---
+++ eloquence_basic_ada/src/main.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO;
+
+procedure Main is
+
+begin
+   --  In the name of GOD
+   Ada.Text_IO.Put_Line("In the name of God");
+   Ada.Text_IO.Put_Line("A date with Ada");
+   null;
+end Main;
